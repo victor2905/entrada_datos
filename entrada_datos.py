47be8d4a4c8c74831a41c776acec474cfc5deb0f
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 22:33:55 -0500

    entrada_datos
    Creación de proyecto

---
+++ entrada_datos.py
@@ -0,0 +1,12 @@
+palabra = input("Introduce una palabra: ")
+num_int = int(input("Introduce un número entero: "))
+num_float = float(input("Introduce un número flotante: "))
+num_complex = complex(input("Introduce un número complejo: "))
+
+print("String: ",palabra)
+print("Entero: ",num_int)
+print("Flotante: ",num_float)
+print("Complejo: ",num_complex)
+
+
+
